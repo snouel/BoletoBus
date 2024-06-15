9395037c1831c72038a263061491f28301733e41
Steven Nouel <89994716+snouelhub@users.noreply.github.com>
2024-06-15 16:18:18 -0400

    fixes

--- BoletoBus/BoletoBus.Api/Program.cs
+++ BoletoBus/BoletoBus.Api/Program.cs
@@ -1,6 +1,19 @@
+using BoletoBus.Infraestructure.Context;
+using BoletoBus.Ioc.Dependencies;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<BoletoBusContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("BoletoBusContext")));
+
+builder.Services.AddAsientoDependency();
+
+
+builder.Services.AddControllers();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
